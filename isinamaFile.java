37b983c6b3f764ab7655d331b829787ef4c2b29c
nabil-arya <nabil_wijatmoko_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:22:47 +0700

    Add files via upload

--- isinamaFile.java
+++ isinamaFile.java
@@ -1,5 +1,13 @@
 public class isinamaFile {
     public static void main(String[] args) {
-        System.out.println("Halo");
+       //main method
+        System.out.println("Nabilarya");
+        System.out.println(x:"SMK telkom Malang");
+        System.out.println(x:"SMP al irsyad piat 7 kota batu");
+        System.out.println(x:"pak wijatmoko");
+        System.out.println(x:"Nama ibu");
+        System.out.println(x:"Programmer");
+        System.out.println(x:"Sepedahan");
     }
+
 }
