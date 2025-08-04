916b746ebd4eb2735e80ba208d1e2c705fb57a10
nabil-arya <nabil_wijatmoko_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:33:35 +0700

    Add files via upload

---
+++ NamaTemandanUsia.java
@@ -0,0 +1,41 @@
+import java.lang.classfile.instruction.StoreInstruction;
+
+public class NamaTemandanUsia {
+    public static void main(String[] args) {
+        String nama1 = "Baihaqqy";
+        int usia1 = 16;
+        String cita1 = "joki drag";
+        System.out.println(nama1);
+        System.out.println(usia1);
+        System.out.println(cita1);
+
+        String nama2 = "Djibril";
+        int usia2 = 12;
+        String cita2 = "Programmer";
+        System.out.println(nama2);
+        System.out.println(usia2);
+        System.out.println(cita2);
+
+        String nama3 = "Radhitya";
+        int usia3 = 16;
+        String cita3 = "Mekanik";
+        System.out.println(nama3);
+        System.out.println(usia3);
+        System.out.println(cita3);
+
+        String nama4 = "Ghaissan";
+        int usia4 = 15;
+        String cita4 = "Softwareengineer";
+        System.out.println(nama4);
+        System.out.println(usia4);
+        System.out.println(cita4);
+
+        String nama5 = "Azka";
+        int usia5 = 16;
+        String cita5 = "  Webdesigner";
+        System.out.println(nama5);
+        System.out.println(usia5);
+        System.out.println(cita5);
+    }
+    
+}
